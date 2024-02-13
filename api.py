a609b4d2ef9126a9d13008b75ad8e3cfcd59b2e2
jasminatarmasheva786 <jasminatarmasheva786@gmail.com>
2024-02-13 14:34:51 +0500

    Finished

--- api.py
+++ api.py
@@ -1,3 +1,32 @@
 import requests
-
+from pprint import pprint
+from tinydb import TinyDB
+from tinydb.database import Document
 base_url = "https://randomuser.me/api/"
+
+db = TinyDB('data.json',indent=4)
+def get_users_by_gender(gender,n):
+    params={
+        "results":n,
+        "gender":gender
+    }
+
+    resp=requests.get(base_url,params=params).json()
+    return resp
+
+def get_users_for_db(gender,n):
+    data=get_users_by_gender(gender,n)
+    us=[]
+    for i in data["results"]:
+        us.append({
+            "first_name": i["name"]["first"],
+            "last_name": i["name"]["last"],
+            "age": i["dob"]["age"],
+            "phone": i["phone"],
+            "country": i["location"]["country"],
+            "email": i["email"],
+        })
+    return us
+
+users=db.table("males")
+pprint(users.insert_multiple(get_users_for_db("females",30)))
